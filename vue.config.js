c52f2c04e9bec94d2ce2bdda8f2b861100d8d641
devfakhzan <fakhzandev@gmail.com>
2022-07-13 06:22:21 +0800

    Reconfigured workbox options

--- vue.config.js
+++ vue.config.js
@@ -9,13 +9,5 @@ module.exports = defineConfig({
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
   }
 })
